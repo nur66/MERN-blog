00545b4e51fc2b3406dea4a5b051eaf23817ed05
unknown <nuriswanto66@gmail.com>
2022-10-18 23:17:42 -0700

    Menambahkan file register.scss

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './register.scss'
 
 const Register = () => {
   return (
-    <div>
-        <p>Register page</p>
+    <div className='main-page'>
+        <div className='left'>
+            <p>Ilustrasi</p>
+        </div>
+        <div className='right'>
+            <p>Form Register</p>
+        </div>
     </div>
   )
 }
