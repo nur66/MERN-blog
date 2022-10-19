89a0997514555c613a516ffe2e85f27f5f73dbc4
unknown <nuriswanto66@gmail.com>
2022-10-19 00:26:29 -0700

    Slicing halaman login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { LoginBg } from '../../assets'
+import { Button, Gap, Input, Link } from '../../component'
 
 const Login = () => {
   return (
-    <div>
-        <p>Login Page</p>
+    <div className='main-page'>
+        <div className='left'>
+            <img src={LoginBg} alt="-background" className="bg-image" />
+        </div>
+        <div className='right'>
+            <p className='title'>Form Login</p>
+            <Input label="Email" placeholder="Email" />
+            <Gap height={18} />
+            <Input label="Password" placeholder="Password" />
+            <Gap height={50} />
+            <Button title="Login" />
+            <Gap height={100} />
+            <Link title="Belum punya akun, silahkan daftar" />
+        </div>
     </div>
   )
 }
