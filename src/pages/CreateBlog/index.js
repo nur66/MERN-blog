2af60900691288cc35e5803ec4005c39f2854c5b
unknown <nuriswanto66@gmail.com>
2022-10-19 20:39:17 -0700

    Slicing Create Blog Page

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Button, Gap, Input, TextArea, Upload } from '../../component';
+import './createBlog.scss';
 
 const CreateBlog = () => {
   return (
-    <div>
-        <p>Create blog page</p>
+    <div className='blog-spot'>
+        <p className='title'>Create blog page</p>
+        <Input label="Post Title" />
+        <Upload />
+        <TextArea />
+        <Gap height={20} />
+        <div className='button-action'>
+            <Button title="Save" />
+        </div>
+        <Gap height={20} />
     </div>
   )
 }
 
-export default CreateBlog
+export default CreateBlog;
